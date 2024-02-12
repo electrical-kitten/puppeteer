b0963867f6de18534396c29a91de446d2587544f
borodinsky <borodinsky.v@gmail.com>
2024-02-12 17:22:14 +0300

    youtube testing

--- pptr/test.js
+++ pptr/test.js
@@ -8,6 +8,12 @@ const searchTermENV = process.env.SEARCHTXT ?? 'defaultValue';
 (async () => {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
+  // await page.setViewport({
+  //   width: 1600,
+  //   height: 1000,
+  //   deviceScaleFactor: 1,
+  // });
+
   await page.goto('https://www.youtube.com/');
   await page.waitForSelector('#search-input #search');
   await page.type('#search-input #search', searchTermCLI, { delay: 100 }); // (id или class элемента, то что будем вводить, задержка ввода что бы видеть)
@@ -17,10 +23,46 @@ const searchTermENV = process.env.SEARCHTXT ?? 'defaultValue';
   // await page.screenshot({ path: './screens/youtubeHome.jpg' });
   await Promise.all([
     page.waitForNavigation(),
+    // page.waitForNetworkIdle(),
     page.click('#search-icon-legacy'),
   ]);
   await page.waitForSelector('ytd-video-renderer h3 a#video-title');
   await page.screenshot({ path: './screens/search-results.jpg' });
+
+  const firstMatch = await page.$eval(
+    'ytd-video-renderer h3 a#video-title',
+    elem => {
+      //runs when that a#video-title is found и возвращает innerText из элемента a#video-title
+      return elem.innerText;
+    }
+  );
+  console.log({ firstMatch });
+  await Promise.all([
+    page.waitForNavigation(),
+    // page.waitForNetworkIdle(),
+    page.click('ytd-video-renderer h3 a#video-title'),
+    new Promise(resolve => setTimeout(resolve, 5000)),
+  ]);
+  await page.screenshot({ path: './screens/searchResultClick.jpg' });
+
+  //get comments number
+  await page.waitForSelector('ytd-comments-header-renderer');
+  const videoComments = await page.$eval(
+    'ytd-comments-header-renderer h2',
+    h2 => {
+      return h2.innerText;
+    }
+  );
+  console.log({ videoComments });
+
+  //get next video suggestion
+  const firstSuggested = await page.$eval(
+    'ytd-comments-header-renderer',
+    elem => {
+      return elem.querySelector('h3').innerText;
+    }
+  );
+  console.log({ firstSuggested });
 
   await browser.close();
 })();
